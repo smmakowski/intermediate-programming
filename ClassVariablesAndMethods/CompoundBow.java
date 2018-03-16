78cc0f377412b4da1795f0d5a6960a9127b12bfd
smmakowski <makowski.sm@gmail.com>
2018-03-16 15:56:54 -0700

    convert floats to doubles; instantiate objs in test class

--- ClassVariablesAndMethods/CompoundBow.java
+++ ClassVariablesAndMethods/CompoundBow.java
@@ -4,25 +4,27 @@ public class CompoundBow {
   public static int numCreated = 0;
   private static ArrayList<String> serials = new ArrayList<String>();
   private String serial;
-  private float ataLength;
+  private double ataLength;
   private int cams;
-  private float weight;
-  private float drawLength;
-  private float speed;
+  private double weight;
+  private double drawLength;
+  private double speed;
 
-  public CompoundBow(int cams, float ataLength,  float weight, float drawLength) {
-    this.serial = "BOW" + numCreated +  (cams > 0 ? (cams > 1 ? "M" : "S") : "N") + // generic serial based off information
-    serials.add(this.serial);
+  public CompoundBow(int cams, double ataLength, double weight, double drawLength) {
+    // set from arguments
     this.ataLength = ataLength;
     this.cams = cams;
     this.weight = weight;
     this.drawLength = drawLength;
+
     this.speed = this.setSpeed(); // set as return value of speed calculations
 
+    this.serial = "BOW" + Integer.toString(numCreated) + (cams > 0 ? (cams > 1 ? "M" : "S") : "N") + Double.toString(this.weight); // generic serial based off information
+    serials.add(this.serial);
     numCreated += 1; // increase num created by one
   }
 
-  public float getWeight() { // instance method to return current weight of instance
+  public double getWeight() { // instance method to return current weight of instance
     return this.weight;
   }
 
@@ -33,15 +35,15 @@ public class CompoundBow {
   public static void about() {
     System.out.println("This method provides basic information for the CompoundBow class, which is designed to emulate generic Compound bows as manufacuted by a .");
     System.out.println("1. Constructors and properties:");
-    System.out.println("\t The CompoundBow constuctor takes 4 arguments... cams (int), ataLength, drawLength, and weight (float).");
-    System.out.println("\t Class also includues additonal instance methods: speed (float), serial, and modelName (String)");
+    System.out.println("\t The CompoundBow constuctor takes 4 arguments... cams (int), ataLength, drawLength, and weight (double).");
+    System.out.println("\t Class also includues additonal instance methods: speed (double), serial, and modelName (String)");
     System.out.println("2. Methods:");
     System.out.println("\t Class includes puclic instance method: instance.getWeight(), and static methods: CompountBow.isValidSerial()");
   }
 
   // private method for internal calcualtion of speed for constructor/set methods
-  private float setSpeed() {
+  private double setSpeed() {
     // this is an arbitrary calculation
-    return ((float)(this.ataLength / 2.0) + (float)(this.drawLength - 8.0)) * (float)(350.0 / this.weight);
+    return ((this.ataLength / 2.0) + (this.drawLength - 8.0)) * (350.0 / this.weight);
   }
 }
---
+++ ClassVariablesAndMethods/CompoundBowTest.java
@@ -0,0 +1,11 @@
+public class CompoundBowTest {
+  public static void main(String[] args) {
+    // made instances with CompoundBow(int cams, float ataLength,  float weight, float drawLength);
+    CompoundBow bow1 = new CompoundBow(2, 32.0, 60.0, 27.0);
+    CompoundBow bow2 = new CompoundBow(2, 38.0, 55.0, 28.5);
+    CompoundBow bow3 = new CompoundBow(1, 40.0, 40.0, 29.25);
+    CompoundBow bow4 = new CompoundBow(0, 36.0, 70.0, 25.4);
+
+
+  }
+}

3f03821e76f16fb1c7ec95bc7f186752ed02c54f
smmakowski <makowski.sm@gmail.com>
2018-03-20 20:59:31 -0700

    implement Bear superclass and SlothBearSubclass

--- SuperKeyword/Bear.java
+++ SuperKeyword/Bear.java
@@ -1,10 +1,51 @@
-public class Bear {
-  // make all
-  public Bear() {
+import java.util.ArrayList;
 
+public class Bear {
+  private String family = "Ursidae";
+  private ArrayList<String> diet = new ArrayList<String>();
+  private String color;
+  private String name;
+
+  public Bear(String name, String color) {
+    this.name = name;
+    this.color = color;
   }
 
   public String toString() {
-    return "{" + "}";
+    return "{family: \"" +  this.family + "\", diet: \"" + this.dietToString() +
+    "\", name: \"" + this.name + "\", color: \"" + this.color + "}";
+  }
+
+  private String dietToString() {
+    String arrayString = "[";
+
+    for (int i = 0; i < this.diet.size() - 1; i++) {
+      arrayString += this.diet.get(i) + ", ";
+    }
+
+    arrayString += this.diet.get(diet.size() -1) + "]";
+
+    return arrayString;
+  }
+
+  public void eat(String food) {
+    if (diet.indexOf(food) == -1) {
+      System.out.print(this.name + " ate the " + food + ".");
+    } else {
+      System.out.println(this.name + " cannot eat " + food + ".");
+    }
+
+  }
+
+  public String roar() {
+    return "Roar! I am a bear!";
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public void addToDiet(String food) {
+    this.diet.add(food);
   }
 }
--- SuperKeyword/SlothBear.java
+++ SuperKeyword/SlothBear.java
@@ -1,10 +1,20 @@
 public class SlothBear extends Bear {
+  // properties unique to the SlothBear Class
+  private int clawLength;
+  private String genusSpecies = "Melursus Ursinus";
 
-  public SlothBear() {
-    super();
+  // constructor
+  public SlothBear(String name, String color, int clawLength) {
+    super(name, color); // calls super constructor
+    this.addToDiet("Termites"); // adds extra food types to base diet
+    this.clawLength = clawLength; // assign unique field with argument
+    // use public set method to reset color
+    this.setColor(color + " with a White spot");
+
   }
 
   public String toString() {
-    return "{" + "}";
+    return super.toString() + "\b, genusSpecies: " + this.genusSpecies + "\b, clawLength: " +
+    this.clawLength +  "}";
   }
 }

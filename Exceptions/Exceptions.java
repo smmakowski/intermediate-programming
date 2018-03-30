01e8038096a9df663d4d45d5f6ea32bba32d06b9
smmakowski <makowski.sm@gmail.com>
2018-03-29 19:22:35 -0700

    add .swp to ignore; handle exceptions in Exceptions class

--- Exceptions/Exceptions.java
+++ Exceptions/Exceptions.java
@@ -1,33 +1,40 @@
 public class Exceptions {
-  public static main(String[] args) throws Exception {
-
+  public static void main(String[] args) {
+    foo();
+    func();
   }
 
-  public static boolean foo() throws Exception {
+  public static void foo() throws IndexOutOfBoundsException {
     try {
       bar(); // calls bar() will not throw expection becase bar can catch on it's own()
     } catch (Exception e) {
-      System.out.println("Function foo() Caught _ exception.");
+      System.out.println("Function foo() Caught Exception thrown by Function bar().");
     }
   }
 
-  public static int bar() throws Exception {
+  public static void bar() throws IndexOutOfBoundsException {
     try {
       // do something (should throw same error as foo() but catch it before foo does)
+      int[] arr = new int[] {4, 3, 2, 1};
+      // for loop intentionally designed to go out of array bounds
+      for (int i = 0; i < 5; i++) {
+        System.out.println(arr[i]);
+      }
     } catch (Exception e) {
-      System.out.println("Function bar() Caught _ exception.");
+      System.out.println("Function bar() Caught _ exception thrown by itself.");
     }
   }
 
-  public static double func() throws Exception {
+  public static void func() throws ArithmeticException {
     try {
-    // should catch error thrown by tion()
+      int num = tion(); // tion() will throw an Exception
     } catch (Exception e) {
-      System.out.println("Function func() Caught _ Exception.");
+      System.out.println("Function func() Caught Exception thrown by Function tion().");
     }
   }
 
-  public static String tion() {
- // do something
+  public static int tion() {
+    // division by zero should throw exception that is caught by func()
+    return 100 / 0;
   }
 }

174eb470545edef753929f9967b869b7aea2c060
smmakowski <makowski.sm@gmail.com>
2018-04-05 15:39:01 -0700

    Add ActionListener delcarations; implement Balance Listener

--- AtmMachine/src/AtmMachine.java
+++ AtmMachine/src/AtmMachine.java
@@ -6,15 +6,24 @@
  */
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class AtmMachine extends JFrame{
-	// constructor to create GUI
+	// GUI component fields
 	private JButton withdrawButton, depositButton, transferButton, balanceButton;
 	private JRadioButton checkingRadio, savingsRadio;
 	private JTextField amountField;
+	// fields for accounts
+	private static Account activeAccount;
+	private static Account checking;
+	private static Account savings;
 	
 	public AtmMachine() {
+		/*
+		 * THE FOLLOW CODE SETS OF THE USER INTERFACE GRID LAYOUT SIZES AND LABELS
+		 */
+		
 		setTitle("ATM Machine");
 		setSize(400, 225);
 		// setMinimumSize(new Dimension(400, 225)); TODO Set minum and Max sizes during final styling
@@ -80,14 +89,70 @@ public class AtmMachine extends JFrame{
         gb.gridwidth = 2; // allow field to take of two cells in a row on layout
         mainPanel.add(amountField, gb);
 	   
+        /*
+         * THE FOLLOWING CODE ADDS ACTION LISTENERS TO BUTTONS AND RADIOBUTTONS
+         */
+        
+       // add Action listeners to buttons
+        withdrawButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+       
+        depositButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) throws NumberFormatException {
+        		try {
+        			double deposit =  Double.parseDouble(amountField.getText());
+        			System.out.println("Valid Deposit");
+        			if (deposit <= 0.0) {
+        				JOptionPane.showMessageDialog(null, "Error: '" + amountField.getText() + 
+        	        	"' is not a number. Please Enter a valid Dollar Amount", "Not a Number", 
+        	        	JOptionPane.ERROR_MESSAGE);
+        			}
+        		} catch (NumberFormatException ex) {
+        			JOptionPane.showMessageDialog(null, "Error: '" + amountField.getText() + 
+        			"' is not a number. Please Enter a valid Dollar Amount", "Not a Number", 
+        			JOptionPane.ERROR_MESSAGE);
+        		}
+           }
+        });
+       
+        transferButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        
+            }
+        });
+        // listener opens OptionPane containing balance for currently selected account when clicked
+        balanceButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		JOptionPane.showMessageDialog(null, "Current balance for '" + activeAccount.getName() +
+        		"' account is:\n\n" + activeAccount.balance(), "Current Balance", JOptionPane.PLAIN_MESSAGE);
+            }
+        });
+        // listener sets active account as checking when triggered
+        checkingRadio.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+        	    activeAccount = checking;
+            }
+        });
+        // listener sets active account as savings when triggered 
+        savingsRadio.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+        	    activeAccount = savings;
+            }
+        });
+        
         // set components as visible
         setVisible(true);
 	}
 	
+	// main methods creates Account Instances and inits GUI
 	public static void main(String[] args) {
 		// instantiate account objects and withdrawal tracker
-		Account checking = new Account();
-		Account savings = new Account();
+		checking = new Account("Checking");
+		savings = new Account("Savings");
+		activeAccount = checking; // set active account as checking (since radio button is checking by default)
 		int withdrawlCount = 0;
 		// Initialize GUI
 		AtmMachine gui = new AtmMachine();

e633666ccf8914422ecd03e0b07753c8962cca2b
smmakowski <makowski.sm@gmail.com>
2018-04-04 19:32:28 -0700

    compile basic GUI

--- GuiProgram/Gui.java
+++ GuiProgram/Gui.java
@@ -1,51 +1,30 @@
 import javax.swing.*;
-import java.awt.Font;
-import java.awt.event.*;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
+import java.awt.*;
 
-public class Gui extends Jframe implements ActionListener {
-  JLabel label;
-  JTextField textField;
-  JButton button;
-  // constructor sets layout for window
+public class Gui extends JFrame {
   public Gui() {
-    this.setSize(600, 400);
-    this.setLocationRelativeTo(null);
-    this.setTitle("Program");
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    JPanel panel = new JPanel(new GridBagLayout());
-		GridBagConstraints gridBag = new GridBagConstraints();
-
-		label = new JLabel("Label: ");
-		ouncesLabel.setFont(new Font("Serif", Font.PLAIN, 28));
-		gridBag.gridx = 0;
-		gridBag.gridy = 0;
-		gridBag.anchor = gridBag.LINE_END;
-		panel.add(label, gridBag);
-
-		textField = new JTextField("", 7);
-		ouncesTextField.setFont(new Font("Serif", Font.PLAIN, 28));
-		gridBag.gridx = 1;
-		gridBag.gridy = 0;
-		gridBag.anchor = gridBag.LINE_START;
-		panel.add(otextField, gridBag);
-
-    button = new JButton("Calculate");
-		calculateButton.setFont(new Font("Serif", Font.PLAIN, 28));
-		calculateButton.addActionListener(this);
-		gridBag.gridx = 1;
-		gridBag.gridy = 4;
-		panel.add(button, gridBag);
-
-		this.add(panel);
-		this.setVisible(true);
+    this("Gui");
   }
-  // event handler
-  public void calculate(ActionEvent event) {
-    GoldPrice gold = new GoldPrice();
-    currentPriceTextField.setText(String.valueOf(gold.currentPrice()));
-    goldValueTextField.setText(String.valueOf(gold.currentPrice() * Double.parseDouble(ouncesTextField.getText())));
+  // constructor sets layout for window
+  public Gui(String t) {
+    setTitle(t);
+    setSize(600, 400);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLocationRelativeTo(null);
+
+    JPanel panel = new JPanel(new GridLayout(4, 2, 0, 10));
+
+		JLabel label = new JLabel("Label: ");
+		panel.add(label);
+
+		JTextField textField = new JTextField("", 7);
+		panel.add(textField);
+
+    JButton button = new JButton("Calculate");
+		// button.addActionListener(this.calculate);
+		panel.add(button);
+
+		add(panel);
+		setVisible(true);
   }
 }

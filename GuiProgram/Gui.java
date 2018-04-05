152c7496c6b199a730e4ffc9d6ca8fc4d3935d98
smmakowski <makowski.sm@gmail.com>
2018-04-04 20:30:01 -0700

    refactor GUI to quadrilateral calculator

--- GuiProgram/Gui.java
+++ GuiProgram/Gui.java
@@ -2,29 +2,46 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Gui extends JFrame {
-  public Gui() {
-    this("Gui");
-  }
   // constructor sets layout for window
-  public Gui(String t) {
-    setTitle(t);
+  public Gui() {
+    setTitle("Quadrilateral Calculator");
     setSize(600, 400);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setLocationRelativeTo(null);
 
-    JPanel panel = new JPanel(new GridLayout(4, 2, 0, 10));
+    JPanel mainPanel = new JPanel(new GridLayout(5, 2));
 
-		JLabel label = new JLabel("Label: ");
-		panel.add(label);
+		JLabel lengthLabel = new JLabel("Length:");
+		mainPanel.add(lengthLabel);
 
-		JTextField textField = new JTextField("", 7);
-		panel.add(textField);
+    JTextField lengthField = new JTextField();
+		mainPanel.add(lengthField);
 
-    JButton button = new JButton("Calculate");
+    JLabel widthLabel = new JLabel("Width:");
+		mainPanel.add(widthLabel);
+
+		JTextField widthField = new JTextField();
+		mainPanel.add(widthField);
+
+    JLabel perimeterLabel = new JLabel("Perimeter:");
+		mainPanel.add(perimeterLabel);
+
+    JTextField perimeterField = new JTextField();
+    mainPanel.add(perimeterField);
+
+    JLabel areaLabel = new JLabel("Area:");
+		mainPanel.add(areaLabel);
+
+    JTextField areaField = new JTextField();
+    mainPanel.add(areaField);
+
+    JLabel messageLabel = new JLabel("");
+    mainPanel.add(messageLabel);
+
+    JButton calculateButton = new JButton("Calculate");
 		// button.addActionListener(this.calculate);
-		panel.add(button);
-
-		add(panel);
+    mainPanel.add(calculateButton);
+		add(mainPanel);
 		setVisible(true);
   }
 }

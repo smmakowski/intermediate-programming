4418c2026c0c1a39f432a1995e9c5389971f6ee7
smmakowski <makowski.sm@gmail.com>
2018-04-20 13:44:53 -0700

    init Project 3

---
+++ Project3/src/Project3Gui.java
@@ -0,0 +1,12 @@
+
+public class Project3Gui {
+	public Project3Gui() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Project3/src/Sequence.java
@@ -0,0 +1,14 @@
+
+public class Sequence {
+	public static computeIterative(int n) {
+		
+	}
+	
+	public static computeRecursive(int n) {
+		
+	}
+	
+	public static getEfficiency() {
+		
+	}
+}

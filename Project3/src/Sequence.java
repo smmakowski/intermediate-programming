cba16325708feab7b4dce44502a715b6f8dacc10
smmakowski <makowski.sm@gmail.com>
2018-04-22 14:46:09 -0700

    implement calculation for iterative method

--- Project3/src/Sequence.java
+++ Project3/src/Sequence.java
@@ -1,8 +1,26 @@
 
 
 public class Sequence {
+	private static int efficiency = 0;
 	public static int computeIterative(int n) {
-		return 0;
+		if (n == 0) {
+			return 0;
+		} else if (n == 1) {
+			return 1;
+		}
+		
+		
+		int previous = 1;
+		int result = 2;
+		int temp;
+		
+		for (int i = 2; i < n; i++) {
+			temp = result;
+			result = (result * 2) + previous;
+			previous = temp;
+		}
+		
+		return result;
 	}
 	
 	public static int computeRecursive(int n) {
@@ -10,6 +28,6 @@ public class Sequence {
 	}
 	
 	public static int getEfficiency() {
-		return 0;
+		return efficiency;
 	}
 }

fb794af7f5336090a07c8d6aea427852d7f73d2d
smmakowski <makowski.sm@gmail.com>
2018-04-22 16:20:40 -0700

    Add headers, complete handler for button and efficiency

--- Project3/src/Project3Gui.java
+++ Project3/src/Project3Gui.java
@@ -1,3 +1,10 @@
+/*
+ * Filename: Project3Gui.java
+ * Author: Stephen Makowski
+ * Date: April 22, 2018
+ * Purpose: Project 3 GUI and file writer
+*/
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -9,6 +16,7 @@ public class Project3Gui extends JFrame {
 	private JButton computeButton;
 	
 	public Project3Gui() {
+		// create window and set gridbag for main panel
 		setTitle("Project 3");
 		setSize(375, 250);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -18,6 +26,7 @@ public class Project3Gui extends JFrame {
 	    getContentPane().add(mainPanel);
 	    GridBagConstraints gb = new GridBagConstraints();
 	    
+	    // add components
 	    iterativeRadio = new JRadioButton("Iterative", true);
 	    gb.gridx = 1;
         gb.gridy = 0;
@@ -29,6 +38,10 @@ public class Project3Gui extends JFrame {
         gb.gridy = 1;
         gb.fill = GridBagConstraints.HORIZONTAL;
         mainPanel.add(recursiveRadio, gb);
+        
+        ButtonGroup radioGroup = new ButtonGroup(); // add radio buttons to group
+        radioGroup.add(iterativeRadio);
+        radioGroup.add(recursiveRadio);
         
         nLabel = new JLabel("Enter n:");
 	    gb.gridx = 0;
@@ -48,6 +61,7 @@ public class Project3Gui extends JFrame {
         gb.fill = GridBagConstraints.HORIZONTAL;
         mainPanel.add(computeButton, gb);
         
+        
         resultLabel = new JLabel("Result:");
 	    gb.gridx = 0;
         gb.gridy = 4;
@@ -58,6 +72,7 @@ public class Project3Gui extends JFrame {
 	    gb.gridx = 1;
         gb.gridy = 4;
         gb.fill = GridBagConstraints.HORIZONTAL;
+        resultField.setEditable(false); // prevent user from editign field
         mainPanel.add(resultField, gb);
         
         efficiencyLabel = new JLabel("Efficiency:");
@@ -70,15 +85,47 @@ public class Project3Gui extends JFrame {
 	    gb.gridx = 1;
         gb.gridy = 5;
         gb.fill = GridBagConstraints.HORIZONTAL;
+        efficiencyField.setEditable(false); // prevent editable by user
         mainPanel.add(efficiencyField, gb);
         
+        // add listener for computer button
+        computeButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) throws NumberFormatException {
+            	// try to calculate and set fields appropriately
+        	    try {
+        	    	int n = Integer.parseInt(nField.getText()); // parse input
+        	    	if (n < 0) { // notify user if input not valid
+        	    		JOptionPane.showMessageDialog(null, "Value entered for 'n' is not a valid number.\nPlease make sure that n is an integer\ngreater than or equal to 0.", 
+        	        	"Invalid Number", JOptionPane.PLAIN_MESSAGE);
+        	    		resultField.setText("");
+        	    		efficiencyField.setText("");
+        	    	} else {
+        	    		int result = 0; // set result to 0
+        	    		if (iterativeRadio.isSelected()) { // run interative if checked
+        	    			result = Sequence.computeIterative(n);
+        	    		} else { // else recursive
+        	    			result = Sequence.computeRecursive(n);
+        	    		}
+        	    		// set fields
+        	    		resultField.setText(Integer.toString(result));
+        	    		efficiencyField.setText(Integer.toString(Sequence.getEfficiency()));
+        	    	}
+        	    } catch (NumberFormatException ex) { // notify user if input not parseable
+        	    	JOptionPane.showMessageDialog(null, "Value entered for 'n' is not a valid number.\nPlease make sure that n is an integer\ngreater than or equal to 0.", 
+        	    	"Invalid Number", JOptionPane.PLAIN_MESSAGE);
+    	    		resultField.setText("");
+    	    		efficiencyField.setText("");
+        	    }
+            }
+        });
+        
+        // set visible
         setVisible(true);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Project3Gui gui = new Project3Gui();
-		
 	}
 
 }
--- Project3/src/Sequence.java
+++ Project3/src/Sequence.java
@@ -1,8 +1,15 @@
-
+/*
+ * Filename: Sequence.java
+ * Author: Stephen Makowski
+ * Date: April 22, 2018
+ * Purpose: Methods for calculating nth term in sequence (iterative and recursive), and efficiency counter/return
+*/
 
 public class Sequence {
-	private static int efficiency = 0;
+	private static int efficiency = 0; // efficiency counter
+	
 	public static int computeIterative(int n) {
+		efficiency = 0; // init efficiency at 0
 		if (n == 0) {
 			return 0;
 		} else if (n == 1) {
@@ -15,6 +22,7 @@ public class Sequence {
 		int temp;
 		
 		for (int i = 2; i < n; i++) {
+			efficiency += 1; // increase efficiency for every iteration of loop
 			temp = result;
 			result = (result * 2) + previous;
 			previous = temp;
@@ -24,21 +32,23 @@ public class Sequence {
 	}
 	
 	public static int computeRecursive(int n) {
+		efficiency = 0; // init efficiency as 0;
+		// if 0th or 1st number in sequence just return since no need to recurse for calcuation
+		if (n == 0) {
+			return 0;
+		} else if (n == 1) {
+			return 1;
+		}
+		
 		return nextSequence(1, 0, 1, n);
 	}
 	
 	public static int getEfficiency() {
-		return efficiency;
+		return efficiency; // returns efficiency
 	}
 	
 	private static int nextSequence(int curr, int prev, int i, int max) {
-		// handle edge cases 0 and 1
-		if (max == 0) {
-			return 0;
-		} else if (max == 1) {
-			return 1;
-		}
-		
+		efficiency += 1;// increment efficiency for eachfunction call
 		// recursive solution
 		if (i == max) { // base case where i has reached max, return final number after calcualtiom
 			return curr;

af17e97bf8a24ac6387a6f9d508d551b51130c95
smmakowski <makowski.sm@gmail.com>
2018-05-04 18:05:32 -0700

    implement toString and courseCompleted methods in Student class

--- GradesDb/src/Student.java
+++ GradesDb/src/Student.java
@@ -1,10 +1,11 @@
 
 public class Student {
+	// instance variables
 	private String name;
 	private String major;
-	private int credits;
-	private int points;
-	
+	private double credits;
+	private double points;
+	//constructor
 	public Student(String name, String major) {
 		this.name = name;
 		this.major = major;
@@ -12,11 +13,40 @@ public class Student {
 		this.points = 0;
 	}
 	
-	public void courseCompleted(String grade, int hours) {
-		
+	public void courseCompleted(String letter, double hours) {
+		double grade = 0.00;
+		// set grade based on letter
+		switch (letter) {
+			case "A":
+				grade = 4.00;
+				break;
+			case "B":
+				grade = 3.00;
+				break;
+			case "C":
+				grade = 2.00;
+				break;
+			case "D":
+				grade = 1.00;
+				break;
+			case "F":
+				grade = 0.00;
+				break;
+			default:
+				// Do Nothing; letter will always be one of above due to JComboBox restrictions
+		}
+		// increment credits and value points
+		this.credits += hours;
+		this.points += (hours * grade); // points for class is (number of hours * gpa for class)
 	}
-	
+	// overidden toString() method; overridden since same method signature
 	public String toString() {
-		return "";
+		double gpa = 4.00;// set gpa to 4.00 automatically
+		if (this.credits > 0) { // calculate gpa if credits earned
+			gpa = points / credits;
+		}
+		// return string
+		return "{name: " + this.name + ", major: " + this.major + ", gpa: " + gpa + "}";
 	}
 }
+ 
